16fecf8dffa37f1937e722ebf3ba464143ca6b6c
swaraj-the-creator <swarajjarak@gmail.com>
2026-09-21 22:09:36 +0530

    initial commit

---
+++ assignment.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number which is going to multiply 1st number which you entered : "))
+d = int(input("Enter a number whose multiples you want to find : "))
+for n in range( 1,n + 1):
+    s = d * n
+    print("\nMultiple = ",s)
---
+++ extra activity.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number to find its multiples till 10 : "))
+mult = 0
+for i in range(5):
+    mult = mult + n 
+    print(mult)
+    
