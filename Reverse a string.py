d37d93412023a08c93b9b67e78ba20602c90d4f8
swaraj-the-creator <swarajjarak@gmail.com>
2026-09-21 16:24:21 +0530

    initial commit

---
+++ Reverse a string.py
@@ -0,0 +1,6 @@
+string = input("Please enter your own string:")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+print("\nThe Orignal String = ",string)
+print("The Reversed String = ",string2)
